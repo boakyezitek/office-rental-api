1754f6e0242f6bb2c03ad50fe7473bcd92ffc369
boakyezitek <kofi.boakye@eziteksolutions.com>
2023-12-25 04:37:10 +0000

    created tag controller and added the tag route, function and test

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -5,62 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Tag;
 use App\Http\Requests\StoreTagRequest;
 use App\Http\Requests\UpdateTagRequest;
+use App\Http\Resources\TagResource;
 
 class TagController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * This method is invoked when the controller is accessed, and it returns
+     * a collection of TagResource instances representing all Tag records.
+     *
+     * @return \App\Http\Resources\TagResourceCollection
      */
-    public function index()
+    public function __invoke()
     {
-        //
+        return TagResource::collection(Tag::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreTagRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Tag $tag)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Tag $tag)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTagRequest $request, Tag $tag)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Tag $tag)
-    {
-        //
-    }
 }
--- database/migrations/2023_12_25_015202_create_tags_table.php
+++ database/migrations/2023_12_25_015202_create_tags_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->timestamps();
-
         });
 
         Tag::create(['name' => 'has_ac']);
