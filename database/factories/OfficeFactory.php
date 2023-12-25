c72a613626bacdbf9a730a4743566a9e1b8d29cb
boakyezitek <kofi.boakye@eziteksolutions.com>
2023-12-25 02:55:10 +0000

    Set up the basic migration table, factories, models, and controller files.

---
+++ database/factories/OfficeFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Office>
+ */
+class OfficeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::facotry(),
+            'title' => $this->fake()->sentence(),
+            'description' => $this->fake()->paragraph(),
+            'lat' => $this->fake()->latitude(),
+            'lng' => $this->fake()->longitude(),
+            'address_line1' => $this->fake()->address(),
+            'address_line2' => $this->fake()->address(),
+            'approval_status' => 2,
+            'hidden' => false,
+            'price_per_day' => $this->fake()->numberBetween(1_000, 2_000),
+            'monthly_discount' => 0
+        ];
+    }
+}
---
+++ database/migrations/2023_12_25_015202_create_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+
+        });
+
+        Tag::create(['name' => 'has_ac']);
+        Tag::create(['name' => 'has_private_bathroom']);
+        Tag::create(['name' => 'has_coffee_machine']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+    }
+};
