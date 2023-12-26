39121d80e12a671613b50f22516784d0a01d5e65
boakyezitek <kofi.boakye@eziteksolutions.com>
2023-12-26 16:13:20 +0000

    configure office resources to hide some attributes

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
 
 class UserResource extends JsonResource
 {
@@ -14,6 +15,8 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return Arr::except(parent::toArray($request), [
+            'created_at', 'updated_at', 'email_verified_at',
+        ]);
     }
 }
