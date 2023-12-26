299d4a3540a3dc721851fd038b51032287a395f2
boakyezitek <kofi.boakye@eziteksolutions.com>
2023-12-26 09:33:27 +0000

    Order by distance but don't include the distance

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -92,9 +92,9 @@ class Office extends Model
     {
         return $builder
         ->select()
-        ->selectRaw('
-        SQRT(POW(69.1 * (lat - ?), 2) + POW(69.1 * (? - lng) * COS(lat / 57.3), 2)) AS distance', [$lat, $lng])
-        ->orderBy('distance');
+        ->orderByRaw('
+        SQRT(POW(69.1 * (lat - ?), 2) + POW(69.1 * (? - lng) * COS(lat / 57.3), 2))', [$lat, $lng]);
+
     }
 
 }
